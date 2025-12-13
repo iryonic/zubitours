f1194e4b79d9bec6bc4c3a9e9953cd16a89bbe40
Irfan Manzoor <thisizirfan@outlook.com>
2025-12-13 11:43:49 +0530

    config setup

--- admin/includes/connection.php
+++ admin/includes/connection.php
@@ -1,7 +1,20 @@
 <?php
+
+// local ENVIRONMENT  setup 
+
+
+// $servername = "localhost";
+// $usename = "root";
+// $password = "";
+// $dbname = "travel_db";
+
+
+// Production ENVIRONMENT  setup 
 $servername = "localhost";
-$usename = "root";
-$password = "";
-$dbname = "travel_db";
+$usename = "u255290550_zubitours";
+$password = "Zubi@1234#";
+$dbname = "u255290550_zubitours";
+
+
 
 $conn = mysqli_connect($servername, $usename, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
