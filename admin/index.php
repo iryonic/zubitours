8215c2714d7bebf9c7fa15fc16315cac295639d7
Irfan Manzoor <thisizirfan@outlook.com>
2026-01-03 13:12:37 +0530

    instant callback cta fixed

--- admin/index.php
+++ admin/index.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login - TravelEase</title>
+    <title>Admin Login </title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         * {
